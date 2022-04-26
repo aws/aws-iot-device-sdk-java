ce60f12f95f8852965ad0edf4bc6278fbf8aaffd
Vera Xia <zhvxia@amazon.com>
2022-04-26 14:38:14 -0700

    remove package for S3 artifacts

--- codebuild/cd/prepareRelease.sh
+++ codebuild/cd/prepareRelease.sh
@@ -8,30 +8,30 @@ set -e
 echo "Compiling source code (version $PKG_VERSION)..."
 mvn -s $HOME/.m2/settings.xml clean package -Dmaven.test.skip=true
 
-echo "Preparing release artifacts..."
-mkdir aws-iot-device-sdk-java-$PKG_VERSION
-pushd aws-iot-device-sdk-java-$PKG_VERSION
-cp ../LICENSE.txt .
-cp ../NOTICE.txt .
-jq --slurp --raw-input '{"text": "\(.)", "mode": "markdown"}' < ../README.md | curl --data @- https://api.github.com/markdown > README.html
-mkdir lib
-cp ../aws-iot-device-sdk-java/target/*.jar lib/
-cp ../aws-iot-device-sdk-java-samples/target/*.jar lib/
-cp -Lr ../aws-iot-device-sdk-java/target/apidocs documentation
-popd
+# echo "Preparing release artifacts..."
+# mkdir aws-iot-device-sdk-java-$PKG_VERSION
+# pushd aws-iot-device-sdk-java-$PKG_VERSION
+# cp ../LICENSE.txt .
+# cp ../NOTICE.txt .
+# jq --slurp --raw-input '{"text": "\(.)", "mode": "markdown"}' < ../README.md | curl --data @- https://api.github.com/markdown > README.html
+# mkdir lib
+# cp ../aws-iot-device-sdk-java/target/*.jar lib/
+# cp ../aws-iot-device-sdk-java-samples/target/*.jar lib/
+# cp -Lr ../aws-iot-device-sdk-java/target/apidocs documentation
+# popd
 
 
 echo "Cleaning up source code for release..."
 mvn -s $HOME/.m2/settings.xml clean
 
 
-echo "Copying clean source code for release..."
-pushd aws-iot-device-sdk-java-$PKG_VERSION
-cp -Lr ../aws-iot-device-sdk-java-samples samples
-pushd samples
-mv samples-pom.xml pom.xml
-popd
-popd
+# echo "Copying clean source code for release..."
+# pushd aws-iot-device-sdk-java-$PKG_VERSION
+# cp -Lr ../aws-iot-device-sdk-java-samples samples
+# pushd samples
+# mv samples-pom.xml pom.xml
+# popd
+# popd
 
 
 # echo "Compressing release artifacts..."

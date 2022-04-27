3dc6b9dc4d02a380f6bd697ca36ee573443a8596
Vera Xia <zhvxia@amazon.com>
2022-04-27 13:46:36 -0700

    clean up the code for java deployment

--- codebuild/cd/test-version-exists.sh
+++ codebuild/cd/test-version-exists.sh
@@ -4,11 +4,9 @@ set -x
 # force a failure if there's no tag
 git describe --tags
 # now get the tag
-#CURRENT_TAG=$(git describe --tags | cut -f2 -dv)
-CURRENT_TAG=0.0.2
+CURRENT_TAG=$(git describe --tags | cut -f2 -dv)
 # convert v0.2.12-2-g50254a9 to 0.2.12
-#CURRENT_TAG_VERSION=$(git describe --tags | cut -f1 -d'-' | cut -f2 -dv)
-CURRENT_TAG_VERSION=0.0.2
+CURRENT_TAG_VERSION=$(git describe --tags | cut -f1 -d'-' | cut -f2 -dv)
 # if there's a hash on the tag, then this is not a release tagged commit
 if [ "$CURRENT_TAG" != "$CURRENT_TAG_VERSION" ]; then
     echo "Current tag version is not a release tag, cut a new release if you want to publish."

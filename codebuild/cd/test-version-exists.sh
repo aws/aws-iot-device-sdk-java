54afb9e6e4726e5feac9a44e0d54b47d84b1b79f
Vera Xia <zhvxia@amazon.com>
2022-04-27 10:42:34 -0700

    Try releasing v1 samples as well

--- codebuild/cd/test-version-exists.sh
+++ codebuild/cd/test-version-exists.sh
@@ -5,10 +5,10 @@ set -x
 git describe --tags
 # now get the tag
 #CURRENT_TAG=$(git describe --tags | cut -f2 -dv)
-CURRENT_TAG=0.0.1
+CURRENT_TAG=0.0.2
 # convert v0.2.12-2-g50254a9 to 0.2.12
 #CURRENT_TAG_VERSION=$(git describe --tags | cut -f1 -d'-' | cut -f2 -dv)
-CURRENT_TAG_VERSION=0.0.1
+CURRENT_TAG_VERSION=0.0.2
 # if there's a hash on the tag, then this is not a release tagged commit
 if [ "$CURRENT_TAG" != "$CURRENT_TAG_VERSION" ]; then
     echo "Current tag version is not a release tag, cut a new release if you want to publish."
